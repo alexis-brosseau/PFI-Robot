cc20491f4cd9117d691622fc302bdd45c26452b8
Tristan Marcotte <tristanmarcotte@outlook.com>
2024-11-23 14:04:47 -0500

    claw class created

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 from motor import Motor
 from window import Window
 from lidar import Lidar
+from claw import Claw
 import numpy as np
 import cv2
 
@@ -20,6 +21,7 @@ class Robot:
         self.cv2 = cv2
         self.window = Window(self.cv2)
         self.lidar = Lidar(self.LIDAR_PORT, self.window)
+        self.claw = Claw()
 
     def __go_forward(self):
         self.motor.change_normal_speed()
